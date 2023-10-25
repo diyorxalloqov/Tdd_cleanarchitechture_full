004f36c77f6b0f22298269de26b57c9e4d205c49
diyorxalloqov <diyorxalloqov@gmail.com>
2023-10-25 09:48:34 +0500

    first commit

--- lib/modules/users/domain/repository/user_repo.dart
+++ lib/modules/users/domain/repository/user_repo.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
-import 'package:tdd_cached_clean_architechture_full_/modules/users/data/model/user_model.dart';
+import 'package:tdd_cached_clean_architechture_full_/modules/users/domain/entity/user_entity.dart';
 import 'package:tdd_cached_clean_architechture_full_/utils/failures.dart';
 
 abstract class UserRepository {
-  Future<Either<Failure, List<UserModel>>> getUsers(int? page);
+  Future<Either<Failure, List<UserEntity>>> getUsers(int? page);
 }
--- lib/modules/users/presentation/bloc/user/user_bloc.dart
+++ lib/modules/users/presentation/bloc/user/user_bloc.dart
@@ -2,7 +2,6 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:tdd_cached_clean_architechture_full_/modules/users/data/model/user_model.dart';
 import 'package:tdd_cached_clean_architechture_full_/modules/users/domain/entity/user_entity.dart';
 import 'package:tdd_cached_clean_architechture_full_/modules/users/domain/usecase/get_users.dart';
 import 'package:tdd_cached_clean_architechture_full_/utils/failures.dart';
@@ -18,7 +17,7 @@ class UserBloc extends Bloc<UserEvent, UserState> {
   UserBloc(this.getUsersUsecase) : super(const UserState()) {
     on<_USersGetEvent>((event, emit) async {
       emit(state.copyWith(status: ActionStatus.isLoading));
-      Either<Failure, List<UserModel>> result = await getUsersUsecase(2);
+      Either<Failure, List<UserEntity>> result = await getUsersUsecase(2);
       result.fold(
           (l) => left(emit(state.copyWith(status: ActionStatus.isFailed))),
           (r) => right(
